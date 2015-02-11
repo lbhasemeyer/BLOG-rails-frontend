10d10b77b3d768c0a2f4720273a5ca746785c437
lkbhas <lbhasemeyer@gmail.com>
2015-02-11 09:20:27 -0700

    fixed issue with boxes on right popping down by floating left let

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -5,8 +5,8 @@ export default Ember.Controller.extend({
     doSearch: function() {
       var input = this.get('term');
       this.store.find('post', {find: input}).then(function (res) {
-          this.set('model', res);
-        }.bind(this));
-      }
+        this.set('model', res);
+      }.bind(this));
+    }
   }
 });
