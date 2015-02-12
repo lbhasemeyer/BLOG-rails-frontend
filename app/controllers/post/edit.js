e43c68af7dc99c624fe2a6287d4da6ae93a3a11e
lkbhas <lbhasemeyer@gmail.com>
2015-02-12 13:52:36 -0700

    adjusted syntax errors ;

--- app/controllers/post/edit.js
+++ app/controllers/post/edit.js
@@ -8,13 +8,13 @@ export default Ember.ObjectController.extend({
       var body = this.get('body');
 
       if (body && title && body.trim() && title.trim()) {
-        post.set('title', title)
-        post.set('body', body)
+        post.set('title', title);
+        post.set('body', body);
         post.save().then(function(){
           this.transitionToRoute('posts');
         }.bind(this));
       }
     }
   }
-  
+
 });
--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   actions: {
     deletePost: function(id) {
-      var post = this.store.find('post', id).then(function(post) {
+      this.store.find('post', id).then(function(post) {
       post.deleteRecord();
       post.save();
       });
